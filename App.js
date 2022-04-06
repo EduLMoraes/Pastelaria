66b1297507e32b901356184c5f6eb82327e32a63
EduardoMoraes <17eduardo05@gmail.com>
2022-04-05 22:45:37 -0300

    main

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'
+import {Text,TouchableOpacity, TextInput, View, H1} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import MeuEstilo from './src/meuestilo'
+
+export default class App extends React.Component{
+  state={
+      pastelCarne:0.0,
+      ca:5.0,
+      pastelQueijo:0.0,
+      pq:3.0,
+      pagar:0.0,
+      pagar2:0.0,
+      pagar3:0.0
+  }
+
+  atualizaPastelCarne=(number)=>{
+      this.setState({pastelCarne:number})
+  }
+
+  atualizaPastelQueijo=(number)=>{
+      this.setState({pastelQueijo:number})
+  }
+  
+  ajuda=()=>{
+    alert("Escolha a seu lanche digitando a quantidade que deseja do mesmo, caso não queira algum dos pastéis listados apenas não preencha")
+  }
+
+  somar=(pastelCarne, ca, pq, pastelQueijo, pagar, pagar2, pagar3)=>{
+     pagar = parseFloat(pastelCarne)*parseFloat(pq)
+     pagar2= parseFloat(pastelQueijo)*parseFloat(ca)
+     pagar3= parseFloat(pagar) + parseFloat(pagar2)
+
+      alert(' Total a pagar: R$'+ pagar3 + ",00")
+  }
+
+  render(){
+      return(
+          <View style={MeuEstilo.container}>
+              
+            <TouchableOpacity style={MeuEstilo.ajuda}
+              onPress={
+                  ()=>this.ajuda()
+              }>
+                  <Text style={MeuEstilo.Text2}>??????HELP???????</Text>
+            </TouchableOpacity>
+
+            <Text style={MeuEstilo.Text}>Pastel de Queijo: R$3,00</Text>
+              <TextInput style={MeuEstilo.input} underlineColorAndroid="transparent"
+              placeholder="0"
+              placeholderTextColor="#9a73ef"
+              autoCapitalize="none"
+              onChangeText={this.atualizaPastelCarne}
+              keyboardType='numeric' />
+
+              <Text style={MeuEstilo.Text}>Pastel de Carne: R$5,00</Text>
+              <TextInput style={MeuEstilo.input} underlineColorAndroid="transparent"
+              placeholder="0"
+              placeholderTextColor="#9a73ef"
+              autoCapitalize="none"
+              onChangeText={this.atualizaPastelQueijo}
+              keyboardType='numeric' />
+
+              <TouchableOpacity style={MeuEstilo.submitButton}
+              onPress={
+                  ()=>this.somar(this.state.pastelCarne, this.state.pastelQueijo, this.state.pq, this.state.ca, this.state.pagar)
+              }>
+                  <Text style={MeuEstilo.submitButtonText}>CONFIRMAR COMPRA</Text>
+              </TouchableOpacity>
+          </View>
+      )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
